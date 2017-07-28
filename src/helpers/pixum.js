9fce61dc81c0dbc95aefeea59c0ad248a036320b
oren bochman <OrenBochman@gmail.com>
2017-07-28 12:52:56 +0300

    moved blog data out of page layout

--- src/helpers/pixum.js
+++ src/helpers/pixum.js
@@ -1,4 +1,4 @@
 
 module.exports = function(x=200,y=200,n=0) {
-  return `<img class="thumbnail"  src="http://lorempixel.com/${x}/${y}/nature/${n}" >`;
+  return `<img class="thumbnail"  src="http://lorempixel.com/${x}/${y}/nature/${n%10}" >`;
 }
